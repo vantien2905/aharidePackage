eef70b398c14702ac7f916ac88cc293ff753fb7d
DinhTien <tiendv@ahamove.com>
2023-02-09 09:36:43 +0700

    Update package file

--- Package.swift
+++ Package.swift
@@ -25,5 +25,17 @@ let package = Package(
         .testTarget(
             name: "aharideTests",
             dependencies: ["aharide"]),
+        .binaryTarget(name: "contacts_service", path: "./Sources/contacts_service.xcframework"),
+        .binaryTarget(name: "App", path: "./Sources/App.xcframework"),
+        .binaryTarget(name: "FlutterPluginRegistrant", path: "./Sources/FlutterPluginRegistrant.xcframework"),
+        .binaryTarget(name: "fluttertoast", path: "./Sources/fluttertoast.xcframework"),
+        .binaryTarget(name: "google_maps_flutter_ios", path: "./Sources/google_maps_flutter_ios.xcframework"),
+        .binaryTarget(name: "location", path: "./Sources/location.xcframework"),
+        .binaryTarget(name: "path_provider_ios", path: "./Sources/path_provider_ios.xcframework"),
+        .binaryTarget(name: "permission_handler_apple", path: "./Sources/permission_handler_apple.xcframework"),
+        .binaryTarget(name: "shared_preferences_ios", path: "./Sources/shared_preferences_ios.xcframework"),
+        .binaryTarget(name: "Toast", path: "./Sources/Toast.xcframework"),
+        .binaryTarget(name: "url_launcher_ios", path: "./Sources/url_launcher_ios.xcframework"),
+        .binaryTarget(name: "webview_flutter_wkwebview", path: "./Sources/webview_flutter_wkwebview.xcframework")
     ]
 )
